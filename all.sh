110bb2935208e0b16ec8d1722719d016a3e7f64a
MAPTECH <138727834+AVEGAH@users.noreply.github.com>
2024-07-21 19:57:17 +0000

    Update all.sh

--- all.sh
+++ all.sh
@@ -243,9 +243,23 @@ while [[ $attempts -ge 0 ]]; do
             break
             ;;
         3)
-            # Call the external passcode verification script
-            ./passcode_verification.sh
-            exit 0
+            passcode_attempts=2
+            while [[ $passcode_attempts -ge 0 ]]; do
+                read -sp "Enter passcode: " passcode
+                echo
+                if [[ "$passcode" == "maptech" ]]; then
+                    echo -e "${GREEN}Passcode verification successful.${NC}"
+                    install_selected_script
+                    exit 0
+                else
+                    invalid_passcode $passcode_attempts
+                    passcode_attempts=$((passcode_attempts - 1))
+                    if [[ $passcode_attempts -lt 0 ]]; then
+                        echo -e "${RED}Too many incorrect passcode attempts. Exiting.${NC}"
+                        exit 1
+                    fi
+                fi
+            done
             ;;
         *)
             invalid_choice $attempts
